6aa141356d474668f113d2c68a9ba862fe46cefc
Saad <66124515+sadsultan@users.noreply.github.com>
2023-08-25 03:24:45 +0500

    Ready to add javascript

---
+++ javascript/app.js
@@ -0,0 +1,4 @@
+import Model from "./store.js";
+import View from "./view.js";
+
+alert("It works!");
---
+++ javascript/store.js
@@ -0,0 +1,5 @@
+export default class Model extends EventTarget {
+    constructor () {
+        super();
+    }
+}
